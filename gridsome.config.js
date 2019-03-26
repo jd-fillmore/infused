2b2fcad7d20a17d41f1ef9bd7355b796154e9468
JD Fillmore <jd@jdfill.me>
2019-03-26 19:14:20 -0400

    update from posts to blog (slug)

--- gridsome.config.js
+++ gridsome.config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       use: '@gridsome/source-filesystem',
       options: {
-        path: 'posts/**/*.md',
+        path: 'blog/**/*.md',
         typeName: 'Post',
         remark: {
           plugins: [
