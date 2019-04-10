1cf5e65aabb0f39b7de6c8f64aa65ed53748ed7c
JD Fillmore <jd@jdfill.me>
2019-04-09 22:57:26 -0400

    thorold

---
+++ src/pages/thorold-web-design.vue
@@ -0,0 +1,254 @@
+<template>
+  <Layout>
+    <!--Hero-->
+    <section class="inner-hero">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-lg-12">
+            <h1>Thorold Web Design</h1>
+            <p>
+              Email us for a free consultation -
+              <a href="mailto:hello@infused.agency">hello@infused.agency</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--Intro-->
+
+    <section class="niche-intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <br>
+            <br>
+            <g-image alt="Thorold web design" src="~/img/thorold-web-design.jpg" class="img-fluid"/>
+            <p>
+              If you're looking for a
+              <a
+                href="https://www.thorold.ca/en/index.aspx"
+                target="_blank"
+              >Thorold</a>
+              <a href="https://en.wikipedia.org/wiki/Web_design" target="_blank"> web design</a> company that knows how to boost your sales and credibility online, you've come to the right place!
+            </p>
+            <p>
+              Web design is super important if you care about your company brand, the immediate first impression you'll have on new customers and more importantly, the ability to sell your company's product or service without having to lift a finger.
+              Web design, in a nut shell, is making sure that when people come to your website, that people can quickly and easily find what they're looking for, and you give them what they need. This is the difference between a web design that does sell, and that doesn't.
+            </p>
+            <h2>Why Your Thorold Business Needs A Web Design</h2>
+            <p>Traditionally in the past, companies didn't have to rely on any sort of online presence, because it didn't exist. However now, since everyone who needs something usually Googles what they need, it's imperative to hire a Thorold web design company that knows what they're doing. You have to make sure that your company is professional represented in a mordern way - so that when people go to your website, they're amazed. They're impressed. And ultimately, your Thorold web design converts them into paying customers.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <p>
+              So, what's the solution?
+              <br>
+              <br>An amazing, modern and easy-to-navigate
+              <a href="/work">web design</a>.
+              <br>
+              <br>When your company has an unbelievable and amazing site, when people come to your website, you no longer have to convince them to buy. With having an amazing site, the amazing impact it will have on people will almost force them to call or email you. So if you have local customers heading over to Google, clicking on your website and viewing your amazing site, those local leads will contact you through your new site, and ultimately you will have more paying customers.
+              <br>
+              <br>You want to make sure that your site is far better than all of your other competition. Because when that is the case, people will be much more impressed and inclined to contact you.
+              <br>
+              <br>You can count on our specialists get the job done for you.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!--Work-->
+
+      <!--Content-->
+      <section id="work">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <h2>Our Web Design Clients In Thorold &amp; Beyond</h2>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <g-image alt="canadian mortgage authority" src="~/img/cma.jpg" class="img-fluid"/>
+              <g-link to="/cma">
+                <button type="submit" class="btn btn-primary">View Case Study</button>
+              </g-link>
+            </div>
+            <div class="col-lg-4">
+              <g-image alt="urban cravings" src="~/img/urban-cravings.jpg" class="img-fluid"/>
+              <g-link to="/urban-cravings">
+                <button type="submit" class="btn btn-primary">View Case Study</button>
+              </g-link>
+            </div>
+            <div class="col-lg-4">
+              <g-image alt="refresh beauty" src="~/img/refresh.jpg" class="img-fluid"/>
+              <g-link to="/refresh-beauty">
+                <button type="submit" class="btn btn-primary">View Case Study</button>
+              </g-link>
+            </div>
+          </div>
+          <div class="row two">
+            <div class="col-lg-4">
+              <g-image alt="chris cutaia" src="~/img/chris.jpg" class="img-fluid"/>
+              <g-link to="/chris-cutaia">
+                <button type="submit" class="btn btn-primary">View Case Study</button>
+              </g-link>
+            </div>
+            <div class="col-lg-4">
+              <g-image
+                alt="sanko construction"
+                src="~/img/sanko-construction.jpg"
+                class="img-fluid"
+              />
+              <g-link to="/sanko-construction">
+                <button type="submit" class="btn btn-primary">View Case Study</button>
+              </g-link>
+            </div>
+            <div class="col-lg-4">&nbsp;</div>
+          </div>
+        </div>
+      </section>
+
+      <br>
+      <br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Our Thorold Web Design Helps Boost Your Credibility &amp; Sales</h2>
+            <p>There are countless benefits as to how site can help not only your business, but really any business. But, lets keep the focus on yours for now.</p>
+            <h4>A 24/7, full times salesperson</h4>
+            <p>
+              Imagine you did not have a website. Better yet, imagine your website had a terrible design and a terrible user experience. Meaning - it wasn't attractive, it wasn't personable and people couldn't find what they're looking for. An overall embarrassment to interact with and look at.
+              <br>
+              <br>Now imagine your website was the total opposite. It looked amazing on devices (desktop, laptop, tablet and phone), it was super easy to navigate, and it was far more beautiful than your competitions.
+              <br>
+              <br>In what situation do you think your potential customers are buying from you?
+              <br>
+              <br>Better design, better user experience = increase in sales.
+            </p>
+            <h4>ROI</h4>
+            <p>
+              The great thing is that everything is translated into
+              <em>trackable</em> results. We're able to track pretty much every aspect of your website - month to month reports that record analytics pertaining to increase in Google rankings, overall traffic and exactly how many conversions you had.
+            </p>
+            <h4>Cost Effectiveness</h4>
+            <p>Investing in a site is bar-none, THE most cost-effective marketing tool in today's modern digital world. The reason for this is that is simple - the sheer impact and potential your website has on the amount of business that it brings in. The inbound marketing nature of a great site saves countless man hours and cost versus something like cold calling, direct mail campaigns and other outbound marketing strategies.</p>
+            <h4>Qualified Leads</h4>
+            <p>
+              This is probably one of the biggest advantages of an amazing site - you get people that
+              <em>need</em> your service, and they're so impressed by what they see that it's a no brainer to contact you. Gone are the days of spending thousands on outbound marketing, in hopes that you
+              <em>might</em> get some customers. Now, when people contact you via your awesome web design, you KNOW that they're interested and that they are absolutely ready to buy.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!--FAQ-->
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h2>FAQ</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <h4>What's the first step?</h4>
+            <p>
+              First, we'll get together for a 10 - 15 minute
+              <a href="/contact">discovery call</a>. This call is used to flesh out the current problems your having (if you already have a website up), and also to talk about your business goals and what you expect your website to do / accomplish.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h4>How does your pricing work?</h4>
+            <p>This completely depends on what you want. For example - is it an informational site? Will you be selling a product or service on the website? Are there any types of custom functionality you wish to use? After these questions are answered, we'll be in a better spot to give you a choice between packages.</p>
+          </div>
+          <div class="col-md-4">
+            <h4>Is there a contract?</h4>
+            <p>Yes. The contract lasts for the length of the project. Once the website is launched and everything is complete, the contract ends. Unless, you want to sign up for website maintenance or security - which is highly recommended.</p>
+          </div>
+        </div>
+        <br><br>
+      </div>
+
+      <section class="need">
+        <div class="container">
+          <div class="row text-center">
+            <div class="col-md-12">
+              <h2>Need a Thorold web design for your business?</h2>
+              <p>Email us today for a free consultation for your new Thorold web design.</p>
+              <a href="mailto:hello@infused.agency">hello@infused.agency</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--History-->
+      <br>
+      <br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>History of Thorold, Ontario</h2>
+            <p>
+              <a
+                href="https://en.wikipedia.org/wiki/Thorold"
+                target="_blank"
+              >Thorold, Ontario</a> is a small city in the Niagara region, located right alongside the Welland Canal. The Canal passes through the heart of the city, and the Twin Flght Locks, which are located downtown. Both of these areas attract thousands of tourists on a yearly basis. The geography of Thorold includes all sorts of neighbourhoods and communities. Some of them being Allanburg, Beaverdams, Confederation Heights and more. St. Johns was one of the very first areas in the inner most area of the Peninsula to be settled by the Europeans. The first Europeans settled in the area way back in the year of 1792, when a small sawmill had been built on the St. John's Creek.
+            </p>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    </section>
+
+    <Footer></Footer>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Thorold Web Design",
+    meta: [
+      {
+        key: "description",
+        name: "description",
+        content:
+          "Thorold web design. At Infused Agency, we help your Thorold-based business come up with a modern and easy to use website design, that will wow your customers."
+      }
+    ]
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../scss/_base.scss";
+
+.inner-hero {
+  a {
+    color: #fff;
+  }
+}
+
+button {
+  width: 100%;
+}
+
+.two {
+  margin-top: 30px;
+}
+
+.need {
+  background: linear-gradient(to right, #2caf73, #0671bc);
+  padding-top: 60px;
+  padding-bottom: 60px;
+  h2,p,a {
+    color: $white;
+  }
+}
+</style>
