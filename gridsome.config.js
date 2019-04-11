b66f09507ce111ce0f5049d7851f7c74a14381a5
JD Fillmore <jd@jdfill.me>
2019-04-11 09:50:05 -0400

    update sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -67,7 +67,7 @@ module.exports = {
       options: {
         cacheTime: 600000, // default
         config: {
-          '/blog*': {
+          '/blog/*': {
             changefreq: 'weekly',
             priority: 0.5
           },
