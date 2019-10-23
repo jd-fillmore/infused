ec48dde9475a47093b74ffa09f9e4471140490b9
JD Fillmore <jd@jdfill.me>
2019-10-22 23:50:07 -0400

    add mc aesthetics

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -5,8 +5,12 @@
       <div class="container">
         <div class="row text-center">
           <div class="col-md-12">
-          <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">Work</h1>
-          <p data-aos="fade-in" data-aos-duration="1000" data-aos-delay="1000">Results we've got for our clients</p>
+            <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">Work</h1>
+            <p
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="1000"
+            >Results we've got for our clients</p>
           </div>
         </div>
       </div>
@@ -17,33 +21,54 @@
         <div class="row">
           <div class="col-lg-4">
             <g-image alt="canadian mortgage authority" src="~/img/cma.jpg" class="img-fluid" />
-            <g-link to="/cma"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-link to="/cma">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
           <div class="col-lg-4">
             <g-image alt="urban cravings" src="~/img/urban-cravings.jpg" class="img-fluid" />
-            <g-link to="/urban-cravings"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-link to="/urban-cravings">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
           <div class="col-lg-4">
-            <g-image alt="refresh beauty" src="~/img/refresh.jpg" class="img-fluid" />
-            <g-link to="/refresh-beauty"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-image alt="mc aesthetics" src="~/img/mc-aesthetics.jpg" class="img-fluid" />
+            <g-link to="/mc-aesthetics">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
         </div>
         <div class="row two">
           <div class="col-lg-4">
             <g-image alt="chris cutaia" src="~/img/chris.jpg" class="img-fluid" />
-            <g-link to="/chris-cutaia"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-link to="/chris-cutaia">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
           <div class="col-lg-4">
             <g-image alt="sanko construction" src="~/img/sanko-construction.jpg" class="img-fluid" />
-            <g-link to="/sanko-construction"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-link to="/sanko-construction">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
           <div class="col-lg-4">
             <g-image alt="krysta gauthier" src="~/img/krysta-gauthier.jpg" class="img-fluid" />
-            <g-link to="/krysta-gauthier-mortgage"><button type="submit" class="btn btn-primary">View Case Study</button></g-link>
+            <g-link to="/krysta-gauthier-mortgage">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
           </div>
         </div>
+        <div class="row two">
+          <div class="col-lg-4">
+            <g-image alt="refresh beauty" src="~/img/refresh.jpg" class="img-fluid" />
+            <g-link to="/refresh-beauty">
+              <button type="submit" class="btn btn-primary">View Case Study</button>
+            </g-link>
+          </div>
+          <div class="col-lg-4">&nbsp;</div>
+          <div class="col-lg-4">&nbsp;</div>
+        </div>
       </div>
-    </div>
     </section>
     <Footer></Footer>
   </Layout>
@@ -69,16 +94,20 @@
     width: 100%;
   }
 }
-
 </style>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Work',
+    title: "Work",
     meta: [
-      { key: 'description', name: 'description', content: 'Check out the detailed case studies based on the results that Infused Agency has put forth for their clients. Click here to view now.' }
+      {
+        key: "description",
+        name: "description",
+        content:
+          "Check out the detailed case studies based on the results that Infused Agency has put forth for their clients. Click here to view now."
+      }
     ]
   }
-}
+};
 </script>
---
+++ src/pages/mc-aesthetics.vue
@@ -0,0 +1,127 @@
+<template>
+  <Layout>
+    <!--Hero-->
+    <section class="inner-hero" data-aos="fade-in" data-aos-duration="1000">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-lg-12">
+            <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">MC Aesthetics</h1>
+            <p
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="1000"
+            >Web Design, Development and Search Engine Optimization</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--Content-->
+    <section class="inner-content">
+      <div class="container" id="shrunk">
+        <div class="row">
+          <div class="col-lg-12">
+            <h2>Challenge</h2>
+            <p>Melissa Cundell from MC Aesthetics approached us through referral. She had heard that we could design and build a great website for her, so that's exactly what we did! She wanted somethin that was modern, clean, pastel coloured and easy to use. Most importantly, she wanted people to email and call her with ease from the website.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <h2>Solution</h2>
+            <p>We talked with Melissa. We discussed how we'd approach the web design, the build itself (using a more powerful platform versus using WordPress), and going over her search engine optimization (SEO) strategy with her. Her new web design below:</p>
+            <g-image
+              alt="mc aesthestics new website"
+              src="~/img/mc-aesthetics-new.jpg"
+              class="img-fluid"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <h2>Result</h2>
+            <p>After less than a week of launching, MC Aesthetics ranks on the first page of Google in multiple cities. Because of this, she's had an influx in new business. It's been a success!</p>
+            <a href="https://mcaesthetics.ca" target="_blank">
+              <button class="btn btn-primary">View Website</button>
+            </a>
+            <br />
+            <br />
+            <p>Here's a screenshot below of MC Aesthetics' SEO in action:</p>
+            <g-image alt="mc aesthetics results" src="~/img/mc-results.jpg" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--CTA-->
+    <section class="pdf">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <h2>Need a website that brings in sales?</h2>
+            <p>Book your free 30 minute consultation. We'll talk about exactly what you need.</p>
+          </div>
+          <div class="col-lg-4">
+            <g-link to="/contact">
+              <button type="button" class="btn btn-primary">Book Now</button>
+            </g-link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer></Footer>
+  </Layout>
+</template>
+
+<style scoped lang="scss">
+@import "../scss/_base.scss";
+
+#shrunk {
+  width: 50%;
+  @include bp-tablet {
+    width: 100%;
+  }
+}
+
+// CTA
+.pdf {
+  background: linear-gradient(to right, #2caf73, #0671bc);
+  padding-top: 60px;
+  padding-bottom: 60px;
+  @include bp-tablet {
+    text-align: center;
+  }
+  .row {
+    align-items: center;
+  }
+  h2,
+  p {
+    color: $white;
+  }
+  h2 {
+    margin-bottom: 0px;
+    @include bp-tablet {
+      margin-bottom: 20px;
+    }
+  }
+  .btn-primary {
+    background: $white;
+    color: $dark-blue;
+  }
+}
+</style>
+
+
+<script>
+export default {
+  metaInfo: {
+    title: "MC Aesthetics Case Study",
+    meta: [
+      {
+        key: "description",
+        name: "description",
+        content:
+          "MC Aesthetics website was designed, developed and SEO'd by Infused Agency. Click here to view the case study."
+      }
+    ]
+  }
+};
+</script>
+
