a84f4f1fb025c48e4cfdb1a9b1fdf17933342476
Fillmore <jfillmore@ehs.com>
2019-10-25 15:52:31 -0400

    img

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,7 +6,11 @@
         <div class="row text-center">
           <div class="col-md-12">
             <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">About</h1>
-            <p data-aos="fade-in" data-aos-duration="1000" data-aos-delay="1000">A bit about our company</p>
+            <p
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="1000"
+            >A bit about our company</p>
           </div>
         </div>
       </div>
@@ -15,14 +19,12 @@
     <section class="inner-content">
       <div class="container">
         <div class="row">
-          <div class="col-md-6 text-center">
-            <g-image alt="jd" src="~/img/jd.jpg" class="img-fluid" />
-            <p>Owner - JD Fillmore</p>
-          </div>
-          <div class="col-md-6">
-            <p>Infused is a results-driven and technologically advanced web design, development &amp; internet marketing agency. We began our journey in the year 2016 in Niagara, Ontario as a startup agency, with a vision of one thing; providing companies with incredible digital experiences and help their businesses flourish.
-            <br><br>
-            We are not in the business of churn &amp; burn projects. We are set on establishing long term growth relationships with our clients. This means doing excellent work, do what we say we will, and get projects done on time. We're always looking out for your best interests first. We're always giving. That's our #1 priority for you.</p>
+          <div class="col-md-12">
+            <p>
+              Infused is a results-driven and technologically advanced web design, development &amp; internet marketing agency. We began our journey in the year 2016 in Niagara, Ontario as a startup agency, with a vision of one thing; providing companies with incredible digital experiences and help their businesses flourish.
+              <br />
+              <br />We are not in the business of churn &amp; burn projects. We are set on establishing long term growth relationships with our clients. This means doing excellent work, do what we say we will, and get projects done on time. We're always looking out for your best interests first. We're always giving. That's our #1 priority for you.
+            </p>
           </div>
         </div>
       </div>
@@ -39,11 +41,16 @@
 <script>
 export default {
   metaInfo: {
-    title: 'About',
+    title: "About",
     meta: [
-      { key: 'description', name: 'description', content: 'Learn more about the core values, hardworking team and how we accomplish your business goals at Infused.' }
+      {
+        key: "description",
+        name: "description",
+        content:
+          "Learn more about the core values, hardworking team and how we accomplish your business goals at Infused."
+      }
     ]
   }
-}
+};
 </script>
 
