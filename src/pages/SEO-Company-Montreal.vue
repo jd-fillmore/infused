ed2c401085de69ac31583b6371a9827149fd061b
JD Fillmore <jd@jdfill.me>
2020-03-16 11:53:33 -0400

    montreal page

---
+++ src/pages/SEO-Company-Montreal.vue
@@ -0,0 +1,322 @@
+<template>
+  <Layout>
+    <!--Hero-->
+    <section class="hero">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h1
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-duration="1000"
+            >SEO Company in Montreal, Agency Services</h1>
+            <h2
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="1000"
+            >We use our SEO services to grow your Montreal business.</h2>
+            <p>Need an SEO company in Montreal that will help your business get seen and bring in revenue? Fill out the form and we'll get back to you ASAP.</p>
+          </div>
+          <div class="col-lg-6">
+            <form
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <div class="form-group">
+                <input
+                  name="name"
+                  type="name"
+                  class="form-control"
+                  id="Name"
+                  aria-describedby="namehelp"
+                  placeholder="Full Name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="email"
+                  type="email"
+                  class="form-control"
+                  id="Email"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="company"
+                  type="name"
+                  class="form-control"
+                  id="Company"
+                  aria-describedby="companyname"
+                  placeholder="Company Name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="url"
+                  type="name"
+                  class="form-control"
+                  id="URL"
+                  aria-describedby="urlhelp"
+                  placeholder="Website URL"
+                />
+              </div>
+              <div class="form-group">
+                <textarea
+                  name="message"
+                  class="form-control"
+                  id="Message"
+                  rows="3"
+                  placeholder="How can we help you?"
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--Content-->
+
+    <section class="niche-intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <br />
+            <br />
+            <br />
+            <h2>Using Infused As Your Montreal SEO Company</h2>
+            <p>When using Infused for your SEO services in Montreal, you won't be disappointed. Lets understand why using us as your Montreal SEO agency is super important for your Montreal business to succeed online.</p>
+            <h3>SEO Promotes Better Cost Management</h3>
+            <p>
+              Every company in Montreal would benefit by using an SEO company.
+              <br />
+              <br />Why?
+              <br />
+              <br />Because once your company is at the top of Google search, you don't have to pay any money to keep it there.
+              <br />
+              <br />As long as you have your website optimized by your Montreal SEO company, it should stay at the top for a very long amount of time.
+              <br />
+              <br />And when your company is at the top of Google search results, you don't have to do any more cold calls, cold emailing or networking events.
+              <br />
+              <br />People wanting your services come directly to you, at no cost.
+            </p>
+
+            <h3>SEO Encourages Local Users To Visit Physical Compay After Searching</h3>
+            <p>
+              A staggering 81% of people use Google to find stores before they actually go into them.
+              <br />
+              <br />If your company doesn't use SEO properly to rank in Google, that means that the vast majority of your untapped customers won't be coming to your store.
+              <br />
+              <br />However, if your Montreal company does use SEO in order to become more visible in Google searches, you will tap into a huge network of customers that will come to your company store.
+            </p>
+
+            <h3>SEO Builds Brand Credibility For Your Montreal Company</h3>
+            <p>
+              If getting new Montreal-based leads wasn't enough for your company, having SEO services set up on your website also helps build your brand's credibility.
+              <br />
+              <br />When people perform a search on Google, and they see your website in the first three results, that gives off the impression that your company is a top player in the Montreal industry.
+              <br />
+              <br />However, if your company is not in the top results, that may tell people that your company is not really established in the Montreal area.
+              <br />
+              <br />Having your website be found in the top results in Google in the Montreal area is imperative or your brand's credibility.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!--Work-->
+
+      <!--Content-->
+      <section id="work">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <h2>Client Case Study</h2>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <g-image
+                alt="SEO Company in Montreal"
+                src="~/img/seo-company-in-montreal.jpg"
+                class="img-fluid"
+              />
+              <br />
+              <br />
+              <p>With Canadian Mortgage Authority, we ranked them on the first page of Google in both organic search results and local map results. Our SEO services can do the same for your Montreal company.</p>
+              <p>
+                <g-link to="/commercial-mortgage-broker-web-design">View the case study of results.</g-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+    <!--Footer-->
+    <section class="hero">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h2
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="1000"
+            >In Montreal and need services an SEO company?</h2>
+            <p>Fill out the form and we'll get back to you with a quote ASAP.</p>
+          </div>
+          <div class="col-lg-6">
+            <form
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <div class="form-group">
+                <input
+                  name="name"
+                  type="name"
+                  class="form-control"
+                  id="Name"
+                  aria-describedby="namehelp"
+                  placeholder="Full Name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="email"
+                  type="email"
+                  class="form-control"
+                  id="Email"
+                  placeholder="Email"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="company"
+                  type="name"
+                  class="form-control"
+                  id="Company"
+                  aria-describedby="companyname"
+                  placeholder="Company Name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  name="url"
+                  type="name"
+                  class="form-control"
+                  id="URL"
+                  aria-describedby="urlhelp"
+                  placeholder="Website URL"
+                />
+              </div>
+              <div class="form-group">
+                <textarea
+                  name="message"
+                  class="form-control"
+                  id="Message"
+                  rows="3"
+                  placeholder="How can we help you?"
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              <input type="hidden" name="bot-field" />
+              <input type="hidden" name="form-name" value="contact" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Footer></Footer>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "SEO Company in Montreal, QC",
+    meta: [
+      {
+        key: "description",
+        name: "description",
+        content:
+          "Need the best SEO company in Montreal that offers SEO services that will help your business bring in more quality leads? Contact us today for a free quote!"
+      }
+    ]
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../scss/_base.scss";
+img {
+  width: 600px;
+}
+.hero {
+  padding-top: 200px;
+  padding-bottom: 200px;
+  background: url("../img/hero-bg.jpg") center center no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  margin-top: 70px;
+  @include bp-tablet-sm {
+    margin-top: 70px;
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+  .btn-primary {
+    box-shadow: none;
+    &:focus,
+    &:hover {
+      box-shadow: none;
+    }
+  }
+  h1 {
+    margin-bottom: 20px;
+    line-height: 1.4;
+    font-size: 16px;
+  }
+  h1,
+  h2,
+  p {
+    color: $white;
+  }
+  h2 {
+    line-height: 1.2;
+    margin-bottom: 20px;
+    font-size: 50px;
+    @include bp-tablet-sm {
+      font-size: 28px;
+    }
+  }
+  p {
+    margin-bottom: 30px;
+    line-height: 1.6;
+  }
+}
+button {
+  width: 100%;
+}
+.two {
+  margin-top: 30px;
+}
+.need {
+  background: linear-gradient(to right, #2caf73, #0671bc);
+  padding-top: 60px;
+  padding-bottom: 60px;
+  h2,
+  p,
+  a {
+    color: $white;
+  }
+}
+</style>
