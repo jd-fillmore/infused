687804a5d4c23e502a6f491fb0b9b64676bcc027
JD Fillmore <jd@jdfill.me>
2020-09-04 15:47:14 -0400

    update design and seo pages

--- src/pages/Seo.vue
+++ src/pages/Seo.vue
@@ -5,39 +5,47 @@
       <div class="container">
         <div class="row text-center">
           <div class="col-lg-12">
-            <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">Search Engine Optimization (SEO)</h1>
-            <p data-aos="fade-in" data-aos-duration="1000" data-aos-delay="1000">Getting you more sales through Google Search</p>
+            <h1
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="500"
+            >Search Engine Optimization (SEO)</h1>
+            <p
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="1000"
+            >Getting you more sales through Google Search</p>
           </div>
         </div>
       </div>
     </section>
-    
-     <!--Intro-->
-     <section class="intro">
+
+    <!--Intro-->
+    <section class="intro">
       <div class="container">
         <div class="row text-center cards">
-        <div class="col-lg-4">
-          <div class="card">
-            <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
-            <h4>Organic SEO</h4>
-            <p>We focus on where the clicks happen - on organic listings. Not paid ads.</p>
+          <div class="col-lg-4">
+            <div class="card">
+              <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
+              <h4>Organic SEO</h4>
+              <p>We focus on where the clicks happen - on organic listings. Not paid ads.</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
+              <h4>Local SEO</h4>
+              <p>Rank high where people Google locally for your business - on Google Maps.</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
+              <h4>Analytics &amp; Reports</h4>
+              <p>Tracking analytics every month to make sure you're getting the highest conversions and ROI.</p>
+            </div>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="card">
-            <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
-            <h4>Content Development</h4>
-            <p>Professionally written and tactfully placed content drivces traffic directly to you.</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card">
-            <g-image alt="check" src="~/img/check.jpg" class="img-fluid mx-auto d-block" />
-            <h4>Analytics &amp; Reports</h4>
-            <p>Tracking analytics every month to make sure you're getting the highest conversions and ROI.</p>
-          </div>
-        </div>
-      </div>
       </div>
     </section>
 
@@ -46,18 +54,119 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <g-image alt="seo search engine optimization" src="~/img/seo.jpg" class="img-fluid mx-auto d-block" />
+            <g-image
+              alt="seo search engine optimization"
+              src="~/img/niagara-web-design.jpg"
+              class="img-fluid cass"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Case Study: Cass-A-Bella Construciton</h2>
           </div>
         </div>
         <div class="row">
           <div class="col-md-6">
-            <p>As a company that specializes in SEO for numerous different industries, know exactly what type of SEO (search engine optimization) strategies it takes to rank these types of business on page one of Google. <br><br>We're different than other search engine optimization businesses because they don't specialize, and know your industry.
-
-            <strong>We do.</strong> We've worked with numerous industries, and have been for years. We know what it takes to succeed online in many different niches.
-            </p>
+            <p>Cass-A-Bella Construction first came to us with needing a new website. Once we launched their website, they wanted one thing - more revenue from it.</p>
+            <br />
+            <p>Within the first month of doing local and non-local SEO for them, we achieved the following results:</p>
           </div>
           <div class="col-md-6">
-            <p>We believe in partnering with you when it comes to growing your business online through search engine optimization. We sit down with you, have weekly meetings to make sure we're hitting your SEO goals. You have touch points all the way from start to end. <br><br>We're also not afraid to share our expertise and guide you to the best possible SEO strategies you can have, that <strong>generates sales.</strong></p>
+            <ul>
+              <li>
+                Organic Google Search views
+                <strong>went up over 320%.</strong>
+              </li>
+              <li>
+                Google Map views
+                <strong>went up almost 100%.</strong>
+              </li>
+              <li>
+                Website visits
+                <strong>went up over 250%.</strong>
+              </li>
+              <li>
+                Within the first month,
+                <strong>three new clients.</strong>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--Pricing-->
+
+    <section id="pricing">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Pricing</h2>
+            <p>Since SEO takes at the very minimum 6-12 months to see any meaningful results, all packages are 6 month packages to start.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="heading">
+              <h2>Beginner</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>499 / mo
+              </h3>
+              <p>For a business wanting to focus on one local area or service to get more leads in</p>
+              <ul>
+                <li>Further optimize site for SEO</li>
+                <li>Competitor research</li>
+                <li>Keyword research</li>
+                <li>Creation of 1 local city / service landing page</li>
+                <li>1 Local citation</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="value">
+              <h4>Best Value, Most Popular</h4>
+            </div>
+            <div class="heading">
+              <h2>Professional</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>999 / mo
+              </h3>
+              <p>For a business wanting to focus on three local areas or services to get more leads in</p>
+              <ul>
+                <li>Further optimize current site for SEO</li>
+                <li>Competitor research</li>
+                <li>Keyword research</li>
+                <li>Create and optimize Google My Business listing</li>
+                <li>Creation of 3 local city / service landing page</li>
+                <li>3 Local citations</li>
+                <li>1 monthly blog post</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="heading">
+              <h2>Enterprise</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>1,999+ / mo
+              </h3>
+              <p>For a business wanting to focus on five local areas or services to get more leads in</p>
+              <ul>
+                <li>Further optimize current site for SEO</li>
+                <li>Competitor research</li>
+                <li>Keyword research</li>
+                <li>Create and optimize Google My Business listing</li>
+                <li>Creation of 5 local city / service landing page</li>
+                <li>5 Local citations</li>
+                <li>2 monthly blog posts</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -83,7 +192,7 @@
           </div>
           <div class="col-lg-3">
             <h3>3. Launch</h3>
-            <p>After we've done our researc, analysis and optimized your website for SEO, we'll launch the systems we've created for you onto the web. We will then submit them to Google.</p>
+            <p>After we've done our research, analysis and optimized your website for SEO, we'll launch the systems we've created for you onto the web. We will then submit them to Google.</p>
           </div>
           <div class="col-lg-3">
             <h3>4. Monthly Efforts</h3>
@@ -93,20 +202,59 @@
       </div>
     </section>
 
+    <!--FAQ-->
+
+    <section id="faq">
+      <div class="container">
+        <div class="row">
+          <div class="col-mg-12">
+            <h2>Frequently Asked Questions</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h3>What does SEO stand for?</h3>
+            <p>Search engine optimization. It's the process of attempting to rank a website high in Google for the search terms that your customers use. If you're on the first page, this is where you will get emails, calls and leads from.</p>
+            <h3>How much does SEO cost?</h3>
+            <p>Please take a look at our three packages above. They vary depending on what you need in terms of SEO, and how competitive your industry is.</p>
+            <h3>How long does SEO take?</h3>
+            <p>This entirely depends on where your website is for certain keywords already. It takes a minimum of 6-12 months to see any meaningful results.</p>
+            <h3>What is the ROI on SEO?</h3>
+            <p>The ROI from SEO is incredible. We've had clients go from zero calls or emails, to within the first week getting consistent leads from their website due to our SEO efforts.</p>
+            <h3>Can you get me to #1 on Google?</h3>
+            <p>No company can guarentee that, because it's ultimately up to Google where they rank you. Having said that, with our list of happy clientele that we've hit #1 with, we're pretty confident we can get you there.</p>
+            <h3>Will my results drop if I stop doing SEO?</h3>
+            <p>It's possible. If you are in a very competitive industry, then yes. If you aren't, then generally no.</p>
+            <h3>I've done SEO before and it didn't work, why are you different?</h3>
+            <p>The chances of it not working is probably because you went with a freelancer or company that did not specialize in SEO, and tried it out on you. Unfortunately this is all too common. However with our tracklist of happy clients that we have and continue to have long-term SEO relationships with, we know what we're doing.</p>
+            <h3>How do we get started?</h3>
+            <p>
+              Fill out the inquiry form on our website, or email us at
+              <a
+                href="mailto:hello@infused.agency"
+              >hello@infused.agency</a> with your name, number and exactly what you're looking for in an SEO campaign.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!--CTA-->
     <section class="pdf">
-     <div class="container">
-       <div class="row">
-         <div class="col-lg-8">
-           <h2>Need SEO to market your company?</h2>
-           <p>Book your free 30 minute consultation. We'll talk about exactly what you need.</p>
-         </div>
-         <div class="col-lg-4">
-           <g-link to="/contact"><button type="button" class="btn btn-primary">Book Now</button></g-link>
-         </div>
-       </div>
-     </div>
-   </section>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <h2>Need SEO to market your company?</h2>
+            <p>Book your free 30 minute consultation. We'll talk about exactly what you need.</p>
+          </div>
+          <div class="col-lg-4">
+            <g-link to="/contact">
+              <button type="button" class="btn btn-primary">Book Now</button>
+            </g-link>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <Footer></Footer>
   </Layout>
@@ -131,12 +279,13 @@
     .card {
       padding: 30px;
       border: none;
-      box-shadow: 0px 20px 100px #EDEFF2;
-      transition: .2s;
-      &:focus, &:hover {
+      box-shadow: 0px 20px 100px #edeff2;
+      transition: 0.2s;
+      &:focus,
+      &:hover {
         box-shadow: 0px 20px 100px #c7c7c7;
         transform: translate3D(0, -5px, 0);
-        transition: .2s;
+        transition: 0.2s;
       }
       @include bp-tablet {
         width: 100%;
@@ -162,8 +311,66 @@
   }
 }
 
-.process {
+// Pricing
+#pricing {
   background: $light-grey;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  @include bp-tablet {
+    padding-top: 60px;
+    padding-bottom: 60px;
+  }
+  .heading {
+    background-image: linear-gradient(to right, #412caf, #0671bc);
+    text-align: center;
+    h2 {
+      color: $white;
+      margin-bottom: 0px;
+      padding-top: 15px;
+      padding-bottom: 15px;
+    }
+  }
+  .value {
+    background: #2ab347;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    h4 {
+      color: $white;
+      margin-bottom: 0px;
+    }
+  }
+  .card {
+    text-align: center;
+    padding: 15px;
+    h3 {
+      margin-bottom: 0px;
+    }
+    p {
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+    ul {
+      list-style: none;
+      margin-left: -40px;
+      li {
+        border-bottom: 1px solid $light-blue;
+        padding-top: 5px;
+        padding-bottom: 5px;
+      }
+    }
+  }
+}
+//FAQ
+#faq {
+  padding-bottom: 100px;
+  @include bp-tablet {
+    padding-top: 60px;
+    padding-bottom: 60px;
+  }
+}
+
+.process {
   padding-top: 100px;
   padding-bottom: 100px;
   @include bp-tablet {
@@ -186,7 +393,8 @@
   .row {
     align-items: center;
   }
-  h2,p {
+  h2,
+  p {
     color: $white;
   }
   h2 {
@@ -200,18 +408,22 @@
     color: $dark-blue;
   }
 }
-
 </style>
 
 
 <script>
 export default {
   metaInfo: {
-    title: 'Search Engine Optimization',
+    title: "Search Engine Optimization",
     meta: [
-      { key: 'description', name: 'description', content: 'Infused offers SEO (search engine optimization) to Niagara businesses and beyond as a business growth strategy. Click here to learn more.' }
+      {
+        key: "description",
+        name: "description",
+        content:
+          "Infused offers SEO (search engine optimization) to Niagara businesses and beyond as a business growth strategy. Click here to learn more."
+      }
     ]
   }
-}
+};
 </script>
 
--- src/pages/WebDesign.vue
+++ src/pages/WebDesign.vue
@@ -5,7 +5,11 @@
       <div class="container">
         <div class="row text-center">
           <div class="col-lg-12">
-            <h1 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">Web Design</h1>
+            <h1
+              data-aos="fade-in"
+              data-aos-duration="1000"
+              data-aos-delay="500"
+            >Web Design &amp; Development</h1>
             <p
               data-aos="fade-in"
               data-aos-duration="1000"
@@ -51,9 +55,9 @@
         <div class="row">
           <div class="col-md-12">
             <g-image
-              alt="sanko construction responsive"
-              src="~/img/residential-landscaping-website-design.jpg"
-              class="img-fluid mx-auto d-block"
+              alt="small &amp; local business website design in niagara ON"
+              src="~/img/niagara-web-design.jpg"
+              class="img-fluid cass"
             />
           </div>
         </div>
@@ -62,8 +66,7 @@
             <p>
               As a leading web design company, you can count on us to design, build and marketing your business specifically.
               <br />
-              <br />
-We're different than other web design and marketing companies because they don't specialize, and know your industry.
+              <br />We're different than other web design and marketing companies because they don't specialize, and know your industry.
               <strong>We do.</strong> We've worked with numerous industries, and have been for years. We know what it takes to succeed online in many different niches.
             </p>
           </div>
@@ -73,6 +76,133 @@ We're different than other web design and marketing companies because they don't
               <br />
               <br />We're also not afraid to share our expertise and guide you to the best possible website you can have, that
               <strong>generates sales.</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--Pricing-->
+
+    <section id="pricing">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Pricing</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="heading">
+              <h2>Beginner</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>1,999
+              </h3>
+              <p>For a business wanting a small website online quickly with minimal functionality</p>
+              <ul>
+                <li>Non-custom design</li>
+                <li>Pre-built WordPress template</li>
+                <li>Ability to edit website</li>
+                <li>Mobile-friendly responsive</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="value">
+              <h4>Best Value, Most Popular</h4>
+            </div>
+            <div class="heading">
+              <h2>Professional</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>2,999
+              </h3>
+              <p>For a business wanting a modern online presence that can bring in leads</p>
+              <ul>
+                <li>Custom, modern design</li>
+                <li>Custom coded</li>
+                <li>Ability to edit website</li>
+                <li>Mobile-friendly responsive</li>
+                <li>Google Analytics</li>
+                <li>Google Search Console</li>
+                <li>Optimized for speed</li>
+                <li>Optimized for performance</li>
+                <li>XML Sitemap</li>
+                <li>Submit website to Google</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="heading">
+              <h2>Enterprise</h2>
+            </div>
+            <div class="card">
+              <h3>
+                <sup>$</sup>4,999+
+              </h3>
+              <p>For a business that needs more than a static website</p>
+              <ul>
+                <li>Modern web application</li>
+                <li>Custom, modern design</li>
+                <li>Custom coded</li>
+                <li>Custom programming</li>
+                <li>Mobile-friendly responsive</li>
+                <li>Optimized for speed</li>
+                <li>Optimized for performance</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--FAQ-->
+    <section id="faq">
+      <div class="container">
+        <div class="row">
+          <div class="col-mg-12">
+            <h2>Frequently Asked Questions</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h3>How much do you charge to build a website?</h3>
+            <p>The pricing above is what you will pay. If you require extra revisions at either the design, development or post-launch stage, we're happy to quote that out for you.</p>
+            <h3>How long will it take to complete my website?</h3>
+            <p>Typically around 3-6 weeks. This is highly dependant on each website, and whether content is ready for the website itself.</p>
+            <h3>Do I have to be in the Niagara region to work with you?</h3>
+            <p>Absolutely not! We've serviced local companies in the Niagara region, to the United States and even Australia.</p>
+            <h3>Will I be able to update the site myself when it's finished?</h3>
+            <p>Absolutely. You will receive a training package that outlines exactly how to edit your website without the knowledge code. We make sure it's a very simple and smooth process.</p>
+            <h3>What is the difference in your website packages?</h3>
+            <p>The vary in complexity and functionality from package to package. If you need something very quick and simple, package one is your best bet. If you want something professional that can bring you in revenue, number two is perfect. If you want anything more complex, number three is your go-to.</p>
+            <h3>Will you maintain our website for us?</h3>
+            <p>If you'd like us to! We intentionally build out websites that allow you to maintain them. But if you're too busy to, we understand and can do it for you.</p>
+            <h3>Do you provide website hosting and domain?</h3>
+            <p>Hosting and domain will be set up by us, but ultimately will be under your control.</p>
+            <h3>How much input do I have in the website deisgn process?</h3>
+            <p>There will be a chance to review the design once we've completed it for you. However with the years of experience our team has in what makes a web design effective for your users, your input should be minimal.</p>
+            <h3>Will my website be mobile-friendly?</h3>
+            <p>Yes! More than 50% of people world-wide browse the web on smart phones, so it only makes sense for your website to be completely mobile-friendly as well.</p>
+            <h3>Who writes the content for the website?</h3>
+            <p>Two options here. You can either write it yourself, or one of our on-staff content writers can write it for you.</p>
+            <h3>Do you only create WordPress websites?</h3>
+            <p>Nope! For websites that don't need a content management system, we typically use other modern technologies to get the job done.</p>
+            <h3>What if I need help on my site down the road?</h3>
+            <p>We'll be here! We pride ourself on a 95% client satisfaction rate since 2016. Long-term client relationships are the key to our business, so we make sure we're always here for you.</p>
+            <h3>Is SEO included in my website build?</h3>
+            <p>For package #2, basic on-page SEO is. Anything outside of that we're happy to quote out for you.</p>
+            <h3>How does payment work?</h3>
+            <p>25% upfront, 25% on us sending over the design, 25% on us sending over the full build for testing to you, and 25% on launch.</p>
+            <h3>How do we get started?</h3>
+            <p>
+              Fill out the inquiry form on our website, or email us at
+              <a
+                href="mailto:hello@infused.agency"
+              >hello@infused.agency</a> with your name, number and exactly what you're looking for in a website.
             </p>
           </div>
         </div>
@@ -154,39 +284,64 @@ We're different than other web design and marketing companies because they don't
 }
 
 // Pricing
-.pricing {
-  .lead-gen {
-    box-shadow: 0px 20px 100px #c7c7c7 !important;
-    transform: translate3D(0, -5px, 0) !important;
+#pricing {
+  background: $light-grey;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  @include bp-tablet {
+    padding-top: 60px;
+    padding-bottom: 60px;
   }
-  ul {
-    list-style: none;
-    padding-left: 0px;
-    li {
-      border-bottom: #c7c7c7 1px solid;
-      padding-top: 10px;
-      padding-bottom: 10px;
-      &.first {
-        background: #2b83e0;
-        color: #fff;
-        border-bottom: none;
+  .heading {
+    background-image: linear-gradient(to right, #412caf, #0671bc);
+    text-align: center;
+    h2 {
+      color: $white;
+      margin-bottom: 0px;
+      padding-top: 15px;
+      padding-bottom: 15px;
+    }
+  }
+  .value {
+    background: #2ab347;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    h4 {
+      color: $white;
+      margin-bottom: 0px;
+    }
+  }
+  .card {
+    text-align: center;
+    padding: 15px;
+    h3 {
+      margin-bottom: 0px;
+    }
+    p {
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+    ul {
+      list-style: none;
+      margin-left: -40px;
+      li {
+        border-bottom: 1px solid $light-blue;
+        padding-top: 5px;
+        padding-bottom: 5px;
       }
     }
   }
-  h3 {
-    &.price {
-      margin-top: 20px;
-    }
-  }
-  p {
-    &.price-text {
-      margin-top: -20px;
-      font-size: 12px;
-      text-transform: uppercase;
-    }
+}
+//FAQ
+#faq {
+  padding-top: 100px;
+  padding-bottom: 100px;
+  @include bp-tablet {
+    padding-top: 60px;
+    padding-bottom: 60px;
   }
 }
-
 // CTA
 .pdf {
   background: linear-gradient(to right, #2caf73, #0671bc);
