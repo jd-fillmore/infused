1c9d828ceecaf9c401886a2ad4e27f7e78389f4b
JD Fillmore <jd@jdfill.me>
2020-11-20 15:17:00 -0500

    adjust clients call

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Nav from "../../components/Nav/nav"
 import Hero from "../../components/Home/Hero/hero"
 import Featured from "../../components/Home/Featured/featured"
 import WhatWeDo from "../../components/Home/WhatWeDo/whatWeDo"
-import Clients from "../../components/Home/Clients/Clients"
+import Clients from "../../components/Home/Clients/clients"
 import CTA from "../../components/CTA/cta"
 import Footer from "../../components/Footer/footer"
 
