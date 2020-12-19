bbabf017562a4f8e7712c4a3dc6af37209d87dab
JD Fillmore <jd@jdfill.me>
2020-12-19 00:28:41 -0500

    garage door repair seo

--- src/pages/seo-keywords-for-garage-door-repair.js
+++ src/pages/seo-keywords-for-garage-door-repair.js
@@ -8,7 +8,7 @@ import InnerContent from "../../components/innerContent/innerContent"
 import CTA from "../../components/CTA/cta"
 import Footer from "../../components/Footer/footer"
 
-import img from "../img/seo-keywords-for-flooring.jpg"
+import img from "../img/seo-keywords-for-garage-door-repair.jpg"
 import Card from "../../components/Card/card"
 import "../pages/about.scss"
 
@@ -16,14 +16,16 @@ const About = () => {
   return (
     <>
       <Helmet>
-        <title>Best SEO Keywords for Flooring &amp; Flooring Companies</title>
+        <title>
+          Best SEO Keywords for Garage Door Repair &amp; Installation Companies
+        </title>
         <meta
           name="description"
-          content="The best SEO keywords for flooring and flooring companies and how we can help you integrate them into your marketing strategy."
+          content="The best SEO keywords for garage door repair or installaiton companies and how we can help you integrate them into your marketing strategy."
         />
       </Helmet>
       <Nav />
-      <InnerHero title="Best SEO Keywords for Flooring &amp; flooring companies" />
+      <InnerHero title="Best SEO Keywords for Garage Door Repair &amp; Installation Companies" />
       <InnerContent>
         <section className="seo-page">
           <Container>
@@ -32,75 +34,73 @@ const About = () => {
                 <img
                   className="img-fluid"
                   src={img}
-                  alt="best seo keywords for flooring, flooring companies"
+                  alt="best seo keywords for garage door repair and installation companies"
                 />
                 <br />
                 <br />
                 <br />
                 <p>
-                  A big part in finding the best SEO keywords for flooring &amp;
-                  flooring companies comes down to a couple things:
+                  A big part in finding the best SEO keywords for garage door
+                  repair companies comes down to a couple things:
                 </p>
                 <ul>
                   <li>
                     What are your customers searching for, in terms of your
-                    flooring?
+                    garage door repair?
                   </li>
                   <li>What city are you located in?</li>
                 </ul>
                 <p>
                   Since local <Link to="/seo">search engine optimization</Link>{" "}
-                  is going to be so huge for you as an flooring or flooring
-                  company (since you offer a local service), it's super
-                  important to build an SEO keyword{" "}
+                  is going to be so huge for you as a garage door repair company
+                  (since you offer a local service), it's super important to
+                  build an SEO keyword{" "}
                   <Link to="/digital-strategy">digital strategy</Link> for your
                   business.{" "}
                 </p>
                 <p>
                   The entire point of SEO is inbound marketing - that is, to
                   bring clients that are looking for what you offer, directly to
-                  you. SEO for flooring or flooring companies works so well
-                  because even when times are tough - recessions, slow times -
-                  it doesn't matter. You're tapping into a market that's outside
-                  of your network (people you don't know), who are looking for
-                  exactly what you offer.
+                  you. SEO for garage door repair or installation companies
+                  works so well because even when times are tough - recessions,
+                  slow times - it doesn't matter. You're tapping into a market
+                  that's outside of your network (people you don't know), who
+                  are looking for exactly what you offer.
                 </p>
                 <p>
                   <strong>
-                    Some of the best keywords for flooring and flooring
-                    companies are:
+                    Some of the best keywords for garage door repair companies
+                    are:
                   </strong>
                   <br />
                   <br />
                   <ul>
+                    <li>"your city + garage door repair"</li>
+                    <li>"your city + garage door repair company" </li>
+                    <li>"your city + overhead garage door repair company"</li>
                     <li>
-                      "your city + flooring" or "your city + flooring company"
+                      "your city + garage door installation" or "your city +
+                      garage door installation company"
                     </li>
-                    <li>"your city + flooring company" </li>
                     <li>
-                      "your city + commercial flooring company" or "your city +
-                      commercial flooring"
+                      "garage door repair near me" or "garage door repair
+                      company near me"
                     </li>
                     <li>
-                      "your city + residential flooring" or "your city +
-                      residential flooring company"
-                    </li>
-                    <li>"flooring near me" or "flooring company near me"</li>
-                    <li>
-                      "best flooring company + your city" or "best flooring +
-                      your city"
+                      "best garage door repair company + your city" or "best
+                      garage door repair + your city"
                     </li>
                   </ul>
                   <p>
                     Lets go a bit indepth below and understand why those are the
-                    best keywords for your flooring or flooring company.
+                    best keywords for your garage door repair company.
                   </p>
                 </p>
                 <br />
                 <Card>
                   <h3 className="text-center">
-                    Want your flooring or flooring company website to rank on
-                    the first page of Google?
+                    Want your garage door repair or garage door installation
+                    website to rank on the first page of Google?
                   </h3>
                   <p className="text-center">
                     We can help you. Get a{" "}
@@ -108,40 +108,45 @@ const About = () => {
                   </p>
                 </Card>
                 <h2>
-                  So.. what are the best SEO keywords for flooring &amp;
-                  flooring companies?
+                  So.. what are the best SEO keywords for garage door repair
+                  &amp; installation companies?
                 </h2>
                 <p>
                   In this short post, we'll go through some SEO keyword ideas
-                  that are the best to use for flooring &amp; flooring companys.
+                  that are the best to use for garage door repair &amp;
+                  installation companies.
                 </p>
-                <h2>"City + flooring" or "city + flooring company"</h2>
+                <h2>
+                  "City + garage door repair" or "city + garage door
+                  installation company"
+                </h2>
                 <p>
                   You'll notice a pattern as we go through these keywords.
                   <br />
                   <br />
-                  Since most flooring &amp; flooring companies deal with clients
-                  locally, you will want to target local people that are
-                  searching for your services.
+                  Since most Garage Door Repair &amp; Installation Companies
+                  deal with clients locally, you will want to target local
+                  people that are searching for your services.
                   <br />
                   <br />
                   If you're in Toronto, you want to show up when someone
-                  searches "Toronto flooring" or "Toronto flooring company".
+                  searches "Toronto garage door repair" or "Toronto garage door
+                  installation".
                   <br />
                   <br />
-                  If you're in Los Angeles, "flooring company LA" or "Los
-                  Angeles flooring".
+                  If you're in Los Angeles, "garage door repair &amp; LA" or
+                  "Los Angeles garage door installation".
                   <br />
                   <br />
                   These are a bit harder to rank for, because they're such
-                  generic terms. Not to mention, since every other flooring or
-                  flooring company is using SEO to try and rank for that
-                  keyword, it's super competitive.
+                  generic terms. Not to mention, since every other garage door
+                  repair or installation company is using SEO to try and rank
+                  for that keyword, it's super competitive.
                   <br />
                   <br />
                   Which leads us to something called a long-tail keyword.
                 </p>
-                <h2>"City + flooring company"</h2>
+                <h2>"City + garage door repair company"</h2>
                 <p>
                   Long tail keywords are usually phrases of four or more words.
                   <br />
@@ -154,13 +159,13 @@ const About = () => {
                   "buyer's intent" when it comes to searching.
                 </p>
                 <h2>
-                  "City + commercial flooring company" or "city + commercial
-                  flooring"
+                  "City + overhead garage door repair company" or "city +
+                  overhead garage door installation company"
                 </h2>
                 <p>
-                  Here, people Google not only for an an flooring or flooring
-                  company within their city.. but a specific type of flooring. A
-                  commercial flooring. See how much more specific, and with that
+                  Here, people Google not only for a garage door repair or
+                  installation company.. but a specific type. An overhead garage
+                  door repair company. See how much more specific, and with that
                   more intention of buying this phrase entails?
                   <br />
                   <br />
@@ -168,22 +173,25 @@ const About = () => {
                   the more intent there is for a person to buy.
                 </p>
                 <h2>
-                  "City + residential flooring" or "city + residential flooring
-                  company"
+                  "City + garage door opener repair" or "city + garage door
+                  opener repair company"
                 </h2>
                 <p>
                   You can see the same sort of effect being played out above.
                   <br />
                   <br />
-                  I'm in Toronto (City) looking to get a residential flooring
-                  work done (flooring) and looking for a firm (residential
-                  flooring company) to do it.
+                  I'm in Toronto (City) looking to get a garage door opener
+                  repair work done (garage door repair) and looking for a
+                  company (garage door opener repair company) to do it.
                   <br />
                   <br />
                   The more specific the search, the more ready-to-buy your
                   customers are.
                 </p>
-                <h2>"flooring near me" or "flooring company near me"</h2>
+                <h2>
+                  "Garage door repair near me" or "garage door repair company
+                  near me"
+                </h2>
                 <p>
                   This one is a bit funny.
                   <br />
@@ -193,7 +201,8 @@ const About = () => {
                   what you offer.
                 </p>
                 <h2>
-                  "Best flooring company + city" or "best flooring + city"
+                  "Best garage door repair company + city" or "best garage door
+                  installation + city"
                 </h2>
                 <p>
                   Not only is there intent in this search, but you know they're
@@ -203,13 +212,13 @@ const About = () => {
                   When someone uses words like "Best", you know that they know
                   that with that, comes money to spend. Which is great for you.
                   In conclusion, these are some of the best SEO keywords for
-                  flooring &amp; flooring companies.
+                  Garage Door Repair Companies.
                 </p>
                 <br />
                 <Card>
                   <h3 className="text-center">
-                    Want your flooring or flooring company website to rank on
-                    the first page of Google?
+                    Want your garage door repair or installation company website
+                    to rank on the first page of Google?
                   </h3>
                   <p className="text-center">
                     We can help you. Get a{" "}
