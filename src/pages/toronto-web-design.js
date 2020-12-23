10ca9defef7e636b058643d0fc28747038533690
JD Fillmore <jd@jdfill.me>
2020-12-22 19:10:47 -0500

    fix image name issues

--- src/pages/toronto-web-design.js
+++ src/pages/toronto-web-design.js
@@ -11,7 +11,7 @@ import TestimonialCard from "../../components/testimonialCard/testimonialCard"
 import ctaSVG from "../../src/img/cta.svg"
 
 import { Helmet } from "react-helmet"
-import featuredImg from "../img/Toronto-web-design.png"
+import featuredImg from "../img/toronto-web-design.png"
 import websiteSVG from "../img/websites.svg"
 import seoSVG from "../img/seo.svg"
 import strategySVG from "../img/strategy.svg"
