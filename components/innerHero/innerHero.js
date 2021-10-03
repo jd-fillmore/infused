cdb3d5bf57bbde7c5a5303e9f18a90e508c16073
JD Fillmore <jd@infused.agency>
2021-10-03 01:32:51 -0400

    add inner hero form

--- components/innerHero/innerHero.js
+++ components/innerHero/innerHero.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Fade from "react-reveal/Fade"
 import { Container, Row, Col } from "reactstrap"
+import Form from "../Form/form"
 
 import "../innerHero/inner-hero.scss"
 
@@ -10,12 +11,16 @@ const InnerHero = ({ title, description }) => {
       <section className="inner-hero text-center">
         <Container>
           <Row>
-            <Col lg="12">
+            <Col lg="6">
               <Fade>
                 <h1>{title}</h1>
                 <hr />
                 <p>{description}</p>
               </Fade>
+            </Col>
+            <Col lg="6">
+              <h4 className="consult">Free Consultation</h4>
+              <Form />
             </Col>
           </Row>
         </Container>
